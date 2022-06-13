b905182f4170249b22a6ee78b6b78e608611d039
chelmerd <chelmerd@student.42wolfsburg.de>
2022-06-13 16:52:04 +0200

    add ex00 - Megaphone

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+namespace megaphone {
+	const std::string	default_msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::string			amplify(char* messages[], size_t msg_count);
+}
+
+std::string	megaphone::amplify(char* messages[], size_t msg_count) {
+	std::string	result = "";
+	for (size_t i = 0; i < msg_count; i++) {
+		std::string	msg = messages[i];
+		for (size_t j = 0; j < msg.length(); j++) {
+			msg[j] = std::toupper(msg[j]);
+		}
+		result += msg;
+	}
+	return result;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		std::cout << megaphone::default_msg << std::endl;
+		return 1;
+	}
+	std::cout << megaphone::amplify(&argv[1], argc - 1) << std::endl;
+	return 0;
+}
